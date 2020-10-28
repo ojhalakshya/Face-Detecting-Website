90e1f70a1dfb5db7381f24ac66b6b192a840788c
ojhalakshya <ojhalakshya@gmail.com>
2020-10-28 18:45:42 +0530

    Adding profile and image feature

--- src/server.js
+++ src/server.js
@@ -40,7 +40,7 @@ app.get("/", (req, res) =>
 app.post("/signin", (req, res) =>
 {
   if(req.body.email === database.users[0].email &&
-      req.body.password == database.users[0].password)
+      req.body.password === database.users[0].password)
   {
     res.json("Success");
   }
@@ -64,7 +64,40 @@ app.post("/register", (req, res) =>
   res.json(database.users[database.users.length - 1]);
 });
 
+// profile
+app.get("/profile/:id", (req, res) =>
+{
+  let { id } = req.params;
+  let found = false;
+  database.users.forEach(user =>
+    {
+      if(user.id === id)
+      {
+        found = true;
+        return res.json(user);
+      }
+    });
+  if(!found)
+  res.status(400).json("No such user");
+});
 
+// Image
+app.put("/image", (req, res) =>
+{
+  let {id} = req.body;
+  let found = false;
+  database.users.forEach(user =>
+    {
+      if(user.id === id)
+      {
+        found = true;
+        user.entries++;
+        return res.json(user.entries);
+      }
+    });
+  if(!found)
+  res.status(400).json("Not Found");
+});
 
 app.listen(3000, () =>
 {
